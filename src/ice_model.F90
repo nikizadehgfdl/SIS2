821e25388a9d9dada5950ccb617d4a254852700a
Niki Zadeh <niki.zadeh@noaa.gov>
2021-09-14 08:24:05 -0400

    Pass flux_u,flux_v to compute_ocean_albedo for white caps parametrization
    - white caps albedo parametrization needs flux_u,flux_v to calculate
        atmos u,v

--- src/ice_model.F90
+++ src/ice_model.F90
@@ -1351,7 +1351,8 @@ subroutine set_ocean_albedo_from_astronomy(Ice, G, Time_start, Time_end)
 
   call compute_ocean_albedo(Ice%ocean_pt, cosz_alb(:,:), Ice%albedo_vis_dir(:,:,1),&
                             Ice%albedo_vis_dif(:,:,1), Ice%albedo_nir_dir(:,:,1),&
-                            Ice%albedo_nir_dif(:,:,1), rad*G%geoLatT(isc:iec,jsc:jec) )
+                            Ice%albedo_nir_dif(:,:,1), rad*G%geoLatT(isc:iec,jsc:jec),&
+                            Ice%flux_u(:,:), Ice%flux_v(:,:))
 
 end subroutine set_ocean_albedo_from_astronomy
 
@@ -1373,7 +1374,8 @@ subroutine set_ocean_albedo_from_coszen(Ice, G, coszen)
 
   call compute_ocean_albedo(Ice%ocean_pt, coszen(isc:iec,jsc:jec), Ice%albedo_vis_dir(:,:,1),&
                             Ice%albedo_vis_dif(:,:,1), Ice%albedo_nir_dir(:,:,1),&
-                            Ice%albedo_nir_dif(:,:,1), rad*G%geoLatT(isc:iec,jsc:jec) )
+                            Ice%albedo_nir_dif(:,:,1), rad*G%geoLatT(isc:iec,jsc:jec),&
+                            Ice%flux_u(:,:), Ice%flux_v(:,:))
 
 end subroutine set_ocean_albedo_from_coszen
 
